e096ffa9e897805358a39c8871fd9569878b0e26
김지수 <kingwltn@gmail.com>
2020-09-10 12:15:43 +0900

    jasoseol Primary Key

--- Jssproject/Jssproject/urls.py
+++ Jssproject/Jssproject/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name = "index"),
+    path('create/', views.create, name = "create"),
+    path('detail/<int:jss_id>', views.detail, name = "detail"),
+    path('delete/<int:jss_id>', views.delete, name = "delete"),
+    path('update/<int:jss_id>', views.update, name = "update"),
+
 ]
